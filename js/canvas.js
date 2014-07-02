71405238c549d71b3819cbaced039bfdbb4133dd
Charlie Greenman <Charlie Greenman>
2014-07-01 22:09:56 -0400

    Initial commit

---
+++ js/canvas.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $("#theTarget").skippr({
+
+
+        transition: 'slide',
+        speed: 1000,
+        easing: 'easeOutQuart',
+        navType: 'block',
+        childrenElementType: 'div',
+        arrows: false,
+        autoPlay: true,
+        autoPlayDuration: 3000,
+        keyboardOnAlways: true,
+        hidePrevious: false
+
+    });
+
+});
+
+
