fbc21781be0a63adc308ec492ebc6365357c199f
razroo <charlie@razroo.com>
2014-09-14 07:59:17 -0400

    decided to remove @font-face as it was causing trouble with the date

--- ice_bucket/js/canvas.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#rotate_bucket').click(function(){
-        $('#bucket').css({
-            'transform': 'rotate(180deg)'
-        });
-    });
-});
--- js/time.js
+++ js/time.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded',domloaded,false);
 function domloaded(){
     // set time you want countdown to end
-    var targetDate  = new Date('August 28, 2014').getTime();
+    var targetDate  = new Date('September 30, 2014').getTime();
     // variables for time units
     var d, h, m, s;
     // input into tag element
-    var countdown = document.getElementById('date');
+    var countdown = document.querySelector('#date span');
+
     // have the code run every second
     setInterval (function () {
         // get amount of second between now and target date
         var currentDate = new Date().getTime();
         var secondsLeft = (targetDate - currentDate) / 1000;
         // determine amount of days, hours, minutes, seconds remaining
-          // as a side note parseInt parses a string and brings it back as an integer
+        // as a side note parseInt parses a string and brings it back as an integer
         d = parseInt(secondsLeft / 86400);
         secondsLeft = secondsLeft % 86400;
 
@@ -21,12 +22,13 @@ function domloaded(){
 
         m = parseInt(secondsLeft / 60);
         s = parseInt(secondsLeft % 60);
+
+
     //format and insert into element
-        countdown.innerHTML ='<p>' + d + '<span>' + 'd, ' + '</span>' +
-                              h + '<span>' + 'h, ' + '</span>' +
-                              m + '<span>' + 'm, ' + '</span>' +
-                              s + '<span>' + 's' + '</span>' + '</p>' +
-                             '<p>'+'until next launch'+'</p>';
+        countdown.innerHTML = d + 'd, ' +
+                              h + 'h, ' +
+                              m + 'm, ' +
+                              s + 's' ;
 
     }, 1000);
 
